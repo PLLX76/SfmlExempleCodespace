ac94e333d589eee17ca87faa82e6ae15f8b39a97
PLLX76 <paul.a.leroy02@gmail.com>
2022-11-17 17:27:12 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# SFML simple template
+Made by PLLX76#8401
+## Run
+to launch your program press the F5 key
+
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <SFML/Network.hpp>
+
+int main(){
+	std::cout << "you code simple with sfml" << std::end;
+}
